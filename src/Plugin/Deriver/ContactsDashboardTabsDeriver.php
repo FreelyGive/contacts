3604b5c69f340327e0e17e30035362de8f8bbd44
yanniboi <yanniboi@1837556.no-reply.drupal.org>
2016-02-19 15:47:08 +0000

    #2671954 by yanniboi: Basic implementation of a Contacts Dashboard.

---
+++ src/Plugin/Deriver/ContactsDashboardTabsDeriver.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\contacts\Plugin\Derivative\ContactsDashboardTabsDeriver.
+ */
+
+namespace Drupal\contacts\Plugin\Deriver;
+
+use Drupal\Component\Plugin\Derivative\DeriverBase;
+use Drupal\Core\Plugin\Context\ContextDefinition;
+
+/**
+ * Provides the contact dashboard tabs with required context.
+ */
+class ContactsDashboardTabsDeriver extends DeriverBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDerivativeDefinitions($base_plugin_definition) {
+    $this->derivatives['contacts_dashboard'] = $base_plugin_definition;
+    $this->derivatives['contacts_dashboard']['admin_label'] = 'Contacts Dashboard Tabs';
+    $this->derivatives['contacts_dashboard']['context'] = [
+      'subpage' => new ContextDefinition('string'),
+      'user' => new ContextDefinition('entity:user'),
+    ];
+
+    return $this->derivatives;
+  }
+
+}
