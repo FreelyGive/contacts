8b004d5cc55398fb66d9bf73a49cd5ede68abfb7
Robert Mumford <rlmumford@googlemail.com>
2017-01-16 13:34:03 +0000

    by rlmumford: Add a custom breadcrumb builder for the party dashboard. (#6)

---
+++ src/BreadcrumbBuilder.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Drupal\contacts;
+
+use Drupal\Core\Breadcrumb\Breadcrumb;
+use Drupal\Core\Breadcrumb\BreadcrumbBuilderInterface;
+use Drupal\Core\Link;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+
+/**
+ * Build the breadcrumbs on the party dashboard.
+ */
+class BreadcrumbBuilder implements BreadcrumbBuilderInterface {
+  use StringTranslationTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function applies(RouteMatchInterface $route_match) {
+    return in_array(
+      $route_match->getRouteName(),
+      [
+        'page_manager.page_view_contacts_dashboard_contact',
+        'page_manager.page_view_contacts_dashboard',
+      ]
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(RouteMatchInterface $route_match) {
+    $breadcrumb = new Breadcrumb();
+    $links = [];
+
+    $links[] = Link::createFromRoute($this->t('Contacts'), 'page_manager.page_view_contacts_dashboard');
+
+    if ($route_match->getRouteName() == 'page_manager.page_view_contacts_dashboard_contact') {
+      $user = $route_match->getParameter('user');
+      $links[] = Link::createFromRoute(
+        $user->label(),
+        'page_manager.page_view_contacts_dashboard_contact',
+        [
+          'user' => $user->id(),
+        ]
+      );
+    }
+
+    return $breadcrumb->setLinks($links);
+  }
+}
