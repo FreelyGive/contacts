64107f88ab11d5ae168d43a4a985309bd0de516d
andrewbelcher <andrewbelcher@655282.no-reply.drupal.org>
2017-01-25 14:02:24 -0500

    Issue #2845223 by andrewbelcher: Finish off theme requirement/install and expand test coverage.

--- tests/src/Functional/ContactsDashboardTest.php
+++ tests/src/Functional/ContactsDashboardTest.php
@@ -36,18 +36,39 @@ class ContactsDashboardTest extends BrowserTestBase {
   }
 
   /**
-   * Function to get information about users from the Admin People view.
-   *
-   * @return mixed
-   *   An associative array of user information or FALSE if not users are found.
+   * Test installing contacts and accessing the contact dashboard.
    */
   public function testViewDashboard() {
-    $this->drupalLogin($this->adminUser);
-
-    $this->drupalGet('admin/contacts');
+    // Check the site has installed successfully.
+    $this->drupalGet('');
     $this->assertSession()->statusCodeEquals(200);
 
-    $this->assertSession()->responseContains('Contacts');
+    // Check that prior to logging in, we can't access the contacts dashboard.
+    $this->drupalGet('admin/contacts');
+    $this->assertSession()->statusCodeEquals(403);
+
+    // Gain access to the contacts dashboard.
+    $this->drupalLogin($this->adminUser);
+
+    // Make sure our items are indexed.
+    /* @var \Drupal\search_api\IndexInterface $index */
+    $index = \Drupal::entityTypeManager()
+      ->getStorage('search_api_index')
+      ->load('contacts_index');
+    $index->indexItems();
+
+    // Check that the contacts dashboard has the expected content.
+    $this->drupalGet('admin/contacts');
+    $session = $this->assertSession();
+    $session->statusCodeEquals(200);
+    $session->elementTextContains('css', '.page-title', 'Contacts');
+
+    // Check our expected users are listed.
+    // @todo: Expand this as we get a proper listing in.
+    $session->elementTextContains('css', '.views-row:nth-child(1) .views-field-uid .field-content', 1);
+    $session->elementTextContains('css', '.views-row:nth-child(1) .views-field-name .field-content', 'admin');
+    $session->elementTextContains('css', '.views-row:nth-child(2) .views-field-uid .field-content', 2);
+    $session->elementTextContains('css', '.views-row:nth-child(2) .views-field-name .field-content', $this->adminUser->getAccountName());
   }
 
 }
