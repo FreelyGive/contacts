a48d51cdfd0a65fd980d310600fb59aa43320769
Yan Loetzer <yan@yanniboi.com>
2017-03-01 03:39:26 -0600

    By yanniboi: Added indiv/org summary and notes tab content to dashboard. (#15)

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -3,10 +3,15 @@
 namespace Drupal\contacts\Controller;
 
 use Drupal\contacts\Ajax\ContactsTab;
+use Drupal\Core\Block\BlockPluginInterface;
+use Drupal\Core\Cache\CacheableMetadata;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Render\Element;
 use Drupal\Core\Url;
 use Drupal\user\UserInterface;
+use Drupal\Core\Plugin\Context\ContextDefinition;
+use Drupal\Core\Plugin\Context\Context;
 
 /**
  * Controller routines for contact dashboard tabs and ajax.
@@ -53,10 +58,37 @@ class DashboardController extends ControllerBase {
   public function renderBlock(UserInterface $user, $subpage) {
     switch ($subpage) {
       case 'summary':
+        $block_manager = \Drupal::service('plugin.manager.block');
+        // @todo Load configuration from somewhere.
+        $config = [
+          'label_display' => 'visible',
+          'label' => 'Individual Summary',
+          'view_mode' => 'default',
+        ];
+        /* @var $plugin_block \Drupal\Core\Block\BlockBase */
+        $plugin_block = $block_manager->createInstance('entity_view:profile', $config);
+        if ($user->hasRole('crm_indiv') && !empty($user->profile_crm_indiv->entity)) {
+          $profile = $user->profile_crm_indiv->entity;
+          $profile_context = new Context(new ContextDefinition('entity:profile', $this->t('Indiv Profile')), $profile);
+          $plugin_block->setContext('entity', $profile_context);
+        }
+        elseif ($user->hasRole('crm_org') && !empty($user->profile_crm_org->entity)) {
+          $profile = $user->profile_crm_org->entity;
+          $profile_context = new Context(new ContextDefinition('entity:profile', $this->t('Org Profile')), $profile);
+          $plugin_block->setContext('entity', $profile_context);
+        }
+        else {
+          // No profile found.
+          $content = ['#markup' => $this->t('Page not found')];
+          break;
+        }
+
+        $block = $this->buildBlockRenderArray($plugin_block);
+
         $content = [
           '#theme' => 'contacts_summary',
           '#content' => [
-            'left' => '<div><h2>Contact Summary</h2><p>This block contains a summary of information about the contact.</p></div>',
+            'left' => $block,
             'right' => '<div><h2>Summary Operations</h2><p>This block contains a list of useful operations to perform on the contact.</p></div>',
           ],
           '#attached' => [
@@ -65,23 +97,34 @@ class DashboardController extends ControllerBase {
         ];
         break;
 
-      case 'indiv':
-        $content = [
-          '#theme' => 'contacts_indiv',
-          '#content' => [
-            'middle' => '<div><h2>Individual Information</h2><p>This block contains information about the individual, such as date of birth and gender.</p></div>',
-          ],
-          '#attached' => [
-            'library' => ['contacts/contacts-dashboard'],
-          ],
-        ];
-        break;
-
       case 'notes':
+        $block_manager = \Drupal::service('plugin.manager.block');
+        // @todo Load configuration from somewhere.
+        $config = [
+          'label_display' => 'visible',
+          'label' => 'Contact Notes',
+        ];
+        /* @var $plugin_block \Drupal\Core\Block\BlockBase */
+        $plugin_block = $block_manager->createInstance('contacts_entity_form', $config);
+        $operation = new Context(new ContextDefinition('string', $this->t('Operation')), 'crm_dashboard');
+        $plugin_block->setContext('operation', $operation);
+
+        if (!empty($user->profile_crm_notes->entity)) {
+          $profile = $user->profile_crm_notes->entity;
+          $profile_context = new Context(new ContextDefinition('entity:profile', $this->t('Notes Profile')), $profile);
+          $plugin_block->setContext('entity', $profile_context);
+        }
+        else {
+          // No profile found.
+          $content = ['#markup' => $this->t('Page not found')];
+          break;
+        }
+        $block = $this->buildBlockRenderArray($plugin_block);
+
         $content = [
           '#theme' => 'contacts_notes',
           '#content' => [
-            'middle' => '<div><h2>Contact Notes</h2><p>This block contains notes made about the contact by staff members.</p></div>',
+            'middle' => $block,
           ],
           '#attached' => [
             'library' => ['contacts/contacts-dashboard'],
@@ -94,6 +137,58 @@ class DashboardController extends ControllerBase {
     }
 
     return $content;
+  }
+
+  /**
+   * Build a render array from a block entity.
+   *
+   * @param \Drupal\Core\Block\BlockPluginInterface $block
+   *   Block entity being rendered.
+   *
+   * @return array
+   *   Render array for the block.
+   */
+  private function buildBlockRenderArray(BlockPluginInterface $block) {
+    if ($block->access(\Drupal::currentUser())) {
+      $block_render_array = [
+        '#theme' => 'block',
+        '#attributes' => [],
+        '#contextual_links' => [],
+        '#weight' => 0,
+        '#configuration' => $block->getConfiguration(),
+        '#plugin_id' => $block->getPluginId(),
+        '#base_plugin_id' => $block->getBaseId(),
+        '#derivative_plugin_id' => $block->getDerivativeId(),
+      ];
+
+      // Build the block and bubble its attributes up if possible. This
+      // allows modules like Quickedit to function.
+      // See \Drupal\block\BlockViewBuilder::preRender() for reference.
+      $content = $block->build();
+      if ($content !== NULL && !Element::isEmpty($content)) {
+        foreach (['#attributes', '#contextual_links'] as $property) {
+          if (isset($content[$property])) {
+            $block_render_array[$property] += $content[$property];
+            unset($content[$property]);
+          }
+        }
+      }
+
+      // If the block is empty, instead of trying to render the block
+      // correctly return just #cache, so that the render system knows the
+      // reasons (cache contexts & tags) why this block is empty.
+      if (Element::isEmpty($content)) {
+        $block_render_array = [];
+        $cacheable_metadata = CacheableMetadata::createFromObject($block_render_array);
+        $cacheable_metadata->applyTo($block_render_array);
+        if (isset($content['#cache'])) {
+          $block_render_array += $content['#cache'];
+        }
+      }
+
+      $block_render_array['content'] = $content;
+      return $block_render_array;
+    }
   }
 
 }
