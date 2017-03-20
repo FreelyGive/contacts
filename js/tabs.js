00131eb87c7fbdc6742909a67632b37af6e6d337
andrewbelcher <andrewbelcher@655282.no-reply.drupal.org>
2017-03-20 19:42:58 +0000

    by andrewbelcher: Break libraries up for more granular future use.

--- js/tabs.js
+++ js/tabs.js
@@ -44,42 +44,4 @@
     });
   }
 
-  /**
-   * @todo: Remove everything after here if https://www.drupal.org/node/2834834
-   * goes in.
-   */
-
-  // Get our original AJAX behavior so we can still call it.
-  var originalAjaxAttach = Drupal.behaviors.AJAX.attach;
-
-  /**
-   * Use AJAX requests for our tabs and update our URL history.
-   *
-   * @type {Drupal~behavior}
-   *
-   * @prop {Drupal~behaviorAttach} attach
-   *   Set our tabs up to use AJAX requests.
-   */
-  Drupal.behaviors.AJAX.attach = function (context, settings) {
-    // Bind Ajax behaviors to all items showing the class.
-    $('.use-ajax').once('ajax').each(function () {
-      var element_settings = {};
-      // Clicked links look better with the throbber than the progress bar.
-      element_settings.progress = {type: 'throbber'};
-
-      // For anchor tags, these will go to the target of the anchor rather
-      // than the usual location.
-      var href = $(this).attr('data-ajax-url') || $(this).attr('href');
-      if (href) {
-        element_settings.url = href;
-        element_settings.event = 'click';
-      }
-      element_settings.base = $(this).attr('id');
-      element_settings.element = this;
-      Drupal.ajax(element_settings);
-    });
-
-    originalAjaxAttach(context, settings);
-  };
-
 })(jQuery, Drupal);
