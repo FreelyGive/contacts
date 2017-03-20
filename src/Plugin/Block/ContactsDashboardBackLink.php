5bcec43a11c9b90e50a7f12ea27aaab681ae7960
andrewbelcher <andrewbelcher@655282.no-reply.drupal.org>
2017-03-20 19:46:28 +0000

    by andrewbelcher: Switch to a block for the back to search link so we can use properly built URLs from routes.

---
+++ src/Plugin/Block/ContactsDashboardBackLink.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Drupal\contacts\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Link;
+use Drupal\Core\Url;
+
+/**
+ * Provides a block to view a contact dashboard summary.
+ *
+ * @Block(
+ *   id = "contacts_back",
+ *   admin_label = @Translation("Back to contacts dashboard link"),
+ *   category = @Translation("Contacts"),
+ * )
+ */
+class ContactsDashboardBackLink extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    return [
+      '#type' => 'link',
+      '#url' => Url::fromRoute('page_manager.page_view_contacts_dashboard'),
+      '#title' => $this->t('Back to search'),
+      '#options' => [
+        'attributes' => [
+          'class' => ['button'],
+        ],
+      ],
+    ];
+  }
+
+}
