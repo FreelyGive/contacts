d8f65f4efb57c23a4001e1e7b7596fc0700b81d2
andrewbelcher <andrewbelcher@655282.no-reply.drupal.org>
2017-04-12 04:00:04 -0400

    install fixces

--- travis-ci/install.sh
+++ travis-ci/install.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 
+echo "# Preparing GIT repos"
+
 # Remove the git details from our repo so we can treat it as a path.
-rm $TRAVIS_BUILD_DIR/.git -rf
+cd $TRAVIS_BUILD_DIR
+rm /.git -rf
+composer required drupal/contacts_theme dev-master --no-update
 
 # Clone contacts_theme and remove the git details so we can treat it as a path.
 git clone --branch=8.x-1.x https://github.com/FreelyGive/contacts_theme.git $DRUPAL_BUILD_ROOT/contacts_theme
 rm $DRUPAL_BUILD_ROOT/contacts_theme/.git -rf
+# @todo: If we require a specific commit, check it out.
 
-# Create our main Drupal Repo.
+# Create our main Drupal project.
+echo "# Creating Drupal project"
 composer create-project drupal-composer/drupal-project:8.x-dev $DRUPAL_BUILD_ROOT/drupal --stability dev --no-interaction --no-install
 cd $DRUPAL_BUILD_ROOT/drupal
 
@@ -16,9 +22,11 @@ composer require drupal/core $DRUPAL_CORE --no-update
 
 # Add our repositories for contacts and contact_theme, as well as re-adding
 # the Drupal package repo.
+echo "# Configuring package repos"
 composer config repositories.0 path $TRAVIS_BUILD_DIR
 composer config repositories.0 path $DRUPAL_BUILD_ROOT/contacts_theme
 composer config repositories.2 composer https://packages.drupal.org/8
 
 # Now require contacts which will pull itself and contacts_theme from the paths.
+echo "# Requiring contacts"
 composer require drupal/contacts dev-master
