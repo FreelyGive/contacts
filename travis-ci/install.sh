e46b859b70ceeb4b33163e8da411f8e55e5bbdb1
yanniboi <yanniboi@1837556.no-reply.drupal.org>
2017-05-08 09:34:44 +0000

    By yanniboi: Remove specific commit and test dependency for contacts_theme.

--- travis-ci/install.sh
+++ travis-ci/install.sh
@@ -5,13 +5,7 @@ echo "# Preparing GIT repos"
 # Remove the git details from our repo so we can treat it as a path.
 cd $TRAVIS_BUILD_DIR
 rm .git -rf
-composer require drupal/contacts_theme dev-master --no-update
-
-# Clone contacts_theme and remove the git details so we can treat it as a path.
-git clone --branch=8.x-1.x https://github.com/FreelyGive/contacts_theme.git $DRUPAL_BUILD_ROOT/contacts_theme
-cd $DRUPAL_BUILD_ROOT/contacts_theme
-rm .git -rf
-# @todo: If we require a specific commit, check it out.
+#composer require drupal/contacts_theme dev-master --no-update
 
 # Create our main Drupal project.
 echo "# Creating Drupal project"
@@ -25,14 +19,12 @@ composer require drupal/coder --no-update --dev
 # We do not need drupal console and drush (required by drupal-project) for tests.
 composer remove drupal/console drush/drush --no-update
 
-# Add our repositories for contacts and contact_theme, as well as re-adding
-# the Drupal package repo.
+# Add our repositories for contacts, as well as re-adding the Drupal package repo.
 echo "# Configuring package repos"
 composer config repositories.0 path $TRAVIS_BUILD_DIR
-composer config repositories.1 path $DRUPAL_BUILD_ROOT/contacts_theme
-composer config repositories.2 composer https://packages.drupal.org/8
+composer config repositories.1 composer https://packages.drupal.org/8
 composer config extra.enable-patching true
 
-# Now require contacts which will pull itself and contacts_theme from the paths.
+# Now require contacts which will pull itself from the paths.
 echo "# Requiring contacts"
 composer require drupal/contacts dev-master
