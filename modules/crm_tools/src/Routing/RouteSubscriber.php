bb1d2f0990aba43572a7fcbab38ec265bcfe0001
Yan Loetzer <yan@yanniboi.com>
2017-11-21 05:19:55 -0500

    by yanniboi: Contacts requires crm_tools.

--- modules/crm_tools/src/Routing/RouteSubscriber.php
+++ modules/crm_tools/src/Routing/RouteSubscriber.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\crm_tools\Routing;
 
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Routing\RouteSubscriberBase;
 use Symfony\Component\Routing\RouteCollection;
 
@@ -11,15 +12,35 @@ use Symfony\Component\Routing\RouteCollection;
 class RouteSubscriber extends RouteSubscriberBase {
 
   /**
+   * The crm_tools configuration object.
+   *
+   * @var \Drupal\Core\Config\ImmutableConfig
+   */
+  protected $config;
+
+  /**
+   * Constructs a new RouteSubscriber.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The config factory service.
+   */
+  public function __construct(ConfigFactoryInterface $config_factory) {
+    $this->config = $config_factory->get('crm_tools.tools');
+  }
+
+  /**
    * {@inheritdoc}
    */
   protected function alterRoutes(RouteCollection $collection) {
-    // Remove the core roles listing.
-    if ($route = $collection->get('entity.user_role.collection')) {
-      $defaults = $route->getDefaults();
-      unset($defaults['_entity_form']);
-      $defaults['_form'] = 'Drupal\crm_tools\Form\OverviewRoles';
-      $route->setDefaults($defaults);
+    // Override the core roles listing with our form.
+    $tools = $this->config->get('active_tools');
+    if (!empty($tools['hats'])) {
+      if ($route = $collection->get('entity.user_role.collection')) {
+        $defaults = $route->getDefaults();
+        unset($defaults['_entity_form']);
+        $defaults['_form'] = 'Drupal\crm_tools\Form\OverviewRoles';
+        $route->setDefaults($defaults);
+      }
     }
   }
 
