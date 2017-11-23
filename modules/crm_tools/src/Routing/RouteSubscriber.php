47c932be9765dd6a4de751791ef5605a2c51cce5
Yan Loetzer <yan@yanniboi.com>
2017-11-23 05:38:32 -0500

    by yanniboi: Removed config form.

--- modules/crm_tools/src/Routing/RouteSubscriber.php
+++ modules/crm_tools/src/Routing/RouteSubscriber.php
@@ -2,7 +2,6 @@
 
 namespace Drupal\crm_tools\Routing;
 
-use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Routing\RouteSubscriberBase;
 use Symfony\Component\Routing\RouteCollection;
 
@@ -12,35 +11,15 @@ use Symfony\Component\Routing\RouteCollection;
 class RouteSubscriber extends RouteSubscriberBase {
 
   /**
-   * The crm_tools configuration object.
-   *
-   * @var \Drupal\Core\Config\ImmutableConfig
-   */
-  protected $config;
-
-  /**
-   * Constructs a new RouteSubscriber.
-   *
-   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
-   *   The config factory service.
-   */
-  public function __construct(ConfigFactoryInterface $config_factory) {
-    $this->config = $config_factory->get('crm_tools.tools');
-  }
-
-  /**
    * {@inheritdoc}
    */
   protected function alterRoutes(RouteCollection $collection) {
     // Override the core roles listing with our form.
-    $tools = $this->config->get('active_tools');
-    if (!empty($tools['hats'])) {
-      if ($route = $collection->get('entity.user_role.collection')) {
-        $defaults = $route->getDefaults();
-        unset($defaults['_entity_form']);
-        $defaults['_form'] = 'Drupal\crm_tools\Form\OverviewRoles';
-        $route->setDefaults($defaults);
-      }
+    if ($route = $collection->get('entity.user_role.collection')) {
+      $defaults = $route->getDefaults();
+      unset($defaults['_entity_form']);
+      $defaults['_form'] = 'Drupal\crm_tools\Form\OverviewRoles';
+      $route->setDefaults($defaults);
     }
   }
 
