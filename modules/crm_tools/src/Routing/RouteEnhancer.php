bd211e3fbe8c8ff584ceefd327ef3ae0994c387a
yanniboi <yanniboi@1837556.no-reply.drupal.org>
2018-03-22 13:47:28 +0000

    By yanniboi: Switch login page with controller to add both login and registration forms.

---
+++ modules/crm_tools/src/Routing/RouteEnhancer.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Drupal\crm_tools\Routing;
+
+use Drupal\Core\Routing\EnhancerInterface;
+use Symfony\Cmf\Component\Routing\RouteObjectInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Route;
+
+/**
+ * Enhancer to unify login and registration pages.
+ */
+class RouteEnhancer implements EnhancerInterface {
+
+  /**
+   * Returns whether the enhancer runs on the current route.
+   *
+   * @param \Symfony\Component\Routing\Route $route
+   *   The current route.
+   *
+   * @return bool
+   */
+  protected function applies(Route $route) {
+    return $route->getPath() == '/user/login';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function enhance(array $defaults, Request $request) {
+    $route = $defaults[RouteObjectInterface::ROUTE_OBJECT];
+    if (!$this->applies($route)) {
+      return $defaults;
+    }
+
+    // @todo Generate a whole new route to prevent changes in core.
+    unset($defaults['_form']);
+    $defaults['_controller'] = '\Drupal\crm_tools\Controller\LoginController::page';
+    return $defaults;
+  }
+
+}
