6a871469fd8d2e95387685111176b58406a72a37
andrewbelcher <andrewbelcher@655282.no-reply.drupal.org>
2018-10-26 16:57:13 +0000

    by andrewbelcher: Fix registration form adjustments.

--- src/Form/RegisterForm.php
+++ src/Form/RegisterForm.php
@@ -18,10 +18,10 @@ class RegisterForm extends CoreRegisterForm {
     /* @var \Drupal\user\UserInterface $entity */
     $entity = parent::getEntityFromRouteMatch($route_match, $entity_type_id);
 
-    // If this is the user login/register route add the individual role.
-    if (in_array($route_match->getRouteName(), ['user.login', 'user.register'])) {
-      $entity->addRole('crm_indiv');
-    }
+    // Add the individual role as the default. Organisations are unlikely to be
+    // created through register forms and in the rare cases they are, roles will
+    // be exposed allowing you to switch.
+    $entity->addRole('crm_indiv');
 
     return $entity;
   }
@@ -32,16 +32,21 @@ class RegisterForm extends CoreRegisterForm {
   public function form(array $form, FormStateInterface $form_state) {
     $form = parent::form($form, $form_state);
 
+    // Core doesn't use the entity's roles if we're registering, but we need to
+    // preserver any roles set by self::getEntityFromRouteMatch, but we do want
+    // to exclude the locked roles.
+    $form['account']['roles']['#default_value'] = $this->entity->getRoles(TRUE);
+
     // If we have the name field (as per our optional install config), make it
     // required depending on the user state..
     if (isset($form['individual:crm_name'])) {
-      if ($this->entity->hasRole('crm_indiv')) {
-        $form['individual:crm_name']['widget'][0]['#required'] = TRUE;
-      }
-      else {
+      if ($form['account']['roles']['#access'] ?? TRUE) {
         $form['individual:crm_name']['#states']['visible'] = [
           ':input[name="roles[crm_indiv]"]' => ['checked' => TRUE],
         ];
+      }
+      elseif ($this->entity->hasRole('crm_indiv')) {
+        $form['individual:crm_name']['widget'][0]['#required'] = TRUE;
       }
     }
 
